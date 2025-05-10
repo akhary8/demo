a74e4396d1ff5b4186d672d06236e513f288dfce
akhary8 <akhary8@gmail.com>
2025-05-10 22:44:32 +0530

    Dto created

---
+++ Ori/src/main/java/com/example/Ori/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.example.Ori.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+	private int id;
+	private String Name;
+	private String Collegename;
+	
+	
+}
