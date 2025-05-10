35e915e2974fe5dfd3bfe342d0e557cc786681f6
akhary <akhary8@gmail.com>
2025-05-10 20:13:20 +0530

    First commit to features repository

---
+++ Ori/src/main/java/com/example/Ori/OriApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OriApplication.class, args);
+	}
+
+}
---
+++ Ori/src/test/java/com/example/Ori/OriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
