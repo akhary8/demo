930c64a742da08f9164ccadab5b5d5120b935ca2
akhary <akhary8@gmail.com>
2025-05-10 20:31:14 +0530

    User entity done by Ak

---
+++ Ori/src/main/java/com/example/Ori/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package com.example.Ori.entity;
+
+
+public class UserEntity {
+	
+	private int id;
+	private String Name;
+	private String College;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getCollege() {
+		return College;
+	}
+	public void setCollege(String college) {
+		College = college;
+	}
+	
+
+}
